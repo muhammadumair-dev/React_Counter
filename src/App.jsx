2408cdde87d690f20d0224f25ae6f065b69cbe94
muhammad umair <m78075257@email.com>
2026-01-28 23:52:53 +0500

    compelte counter app

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import {ChevronUp} from 'lucide-react'
+import {ChevronDown} from 'lucide-react'
+
+function App() {
+  const [num, setnum] = useState(0)
+  function   increase(){
+  setnum(num+1)
+    
+  }
+  function decrease(){
+   setnum(num-1)
+    
+
+  }
+    function jumpfive(){
+   setnum(num+5)
+    
+
+  }
+  
+
+  return (
+    <div className='flex hover:scale-110 transition-transform duration-300 shadow-md hover:shadow-xl transition-shadow duration-300 flex-col rounded-full justify-self-center self-center my-40  items-center w-fit bg-blue-900'>
+      <div onClick={increase} className='active:scale-95 transition-transform duration-150"'><ChevronUp size={120} color='white' /></div>
+      <h1 className='text-8xl text-amber-50'>{num}</h1>
+      <div onClick={decrease} className='active:scale-95 transition-transform duration-150"'><ChevronDown size={120} color='white'  /></div>
+  
+    </div>
+  
+  )
+}
+
+export default App
